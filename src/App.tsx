b9f843da8b5294b4d40cdc30c4cde4079066a58c
trojan0523 <1153532327@qq.com>
2023-09-03 12:55:03 +0800

    feat: change state

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,19 @@
  * @Author: BuXiongYu
  * @Date: 2023-08-24 14:04:39
  * @LastEditors: BuXiongYu
- * @LastEditTime: 2023-08-25 19:20:18
+ * @LastEditTime: 2023-09-03 12:54:51
  * @Description: 请填写简介
  */
-import { useEffect, useState } from 'react'
 import TabsComponent from './tabs'
-import { Button } from 'react-vant'
 import './App.css'
 
 function App() {
-  const [flag, setFlag] = useState<boolean>(false)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setFlag(true)
-    }, 2500)
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [])
 
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-        <Button style={{ width: '120px' }} type='primary' onClick={() => setFlag(!flag)}>切换</Button>
       </div>
-      <TabsComponent showBodySizeFlag={flag} />
+      <TabsComponent />
     </>
   )
 }
--- src/tabs.tsx
+++ src/tabs.tsx
@@ -2,19 +2,27 @@
  * @Author: BuXiongYu
  * @Date: 2023-08-24 14:23:41
  * @LastEditors: BuXiongYu
- * @LastEditTime: 2023-08-25 19:20:21
+ * @LastEditTime: 2023-09-03 12:54:33
  * @Description: 请填写简介
  */
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Tabs } from 'react-vant'
 import './tabs.css'
 
-const TabsComponent = (props: { showBodySizeFlag: boolean }) => {
-  const { showBodySizeFlag } = props
+const TabsComponent = () => {
+  const [showBodySizeFlag, setShowBodySizeFlag] = useState<boolean>(false)
   const [tab, setTab] = useState<string>('BODY_SIZE')
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowBodySizeFlag(true)
+    }, 2500)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
   const TabList = useMemo(() => {
-    console.log('execute TabComponents', showBodySizeFlag)
     const originalTabList = [
       {
         key: 'BODY_SIZE',
@@ -28,16 +36,16 @@ const TabsComponent = (props: { showBodySizeFlag: boolean }) => {
         key: 'PRODUCT_SIZE',
         component: (
           <>
-            <div>PRODUCT_SIZE</div>  
+            <div>PRODUCT_SIZE</div>
           </>
         )
       },
     ]
     if (!showBodySizeFlag) {
-      const filterList = originalTabList.filter((item) => item.key === 'PRODUCT_SIZE')
+      const filterList = originalTabList.filter((item) => item.key === 'BODY_SIZE')
       return filterList
     }
-    setTab('BODY_SIZE')
+    setTab('PRODUCT_SIZE')
     return originalTabList
   }, [showBodySizeFlag])
 
@@ -53,7 +61,7 @@ const TabsComponent = (props: { showBodySizeFlag: boolean }) => {
       setTab('PRODUCT_SIZE')
     }
   }, [showBodySizeFlag])
-  
+
   return (
     <div className='size-tab-list'>
       <Tabs
@@ -71,7 +79,6 @@ const TabsComponent = (props: { showBodySizeFlag: boolean }) => {
         stickyInitScrollbar={true}
         animated
         border
-        // key={TabList.length}
       >
         {
           TabList.map((item) => (
